7bb6ae28af9272ab8e027fada2dfe3944f6fdd1d
Abilash <abilash2408@gmail.com>
2024-07-01 12:42:44 +0530

    edit

--- payroll/src/main/java/com/shard/payroll/PayrollApplication.java
+++ payroll/src/main/java/com/shard/payroll/PayrollApplication.java
@@ -2,9 +2,9 @@ package com.shard.payroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+
+@SpringBootApplication
 public class PayrollApplication {
 
 	public static void main(String[] args) {
